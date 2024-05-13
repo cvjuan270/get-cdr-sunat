ebaf784c08690f6a90247589c41a55946d05d309
Juan Diego <juand@DESKTOP-7NGM793>
2024-05-13 16:13:21 -0500

    initial

---
+++ cdr_sunat_service.py
@@ -0,0 +1,109 @@
+# Module Name: my_module.py
+
+import os
+import base64
+import zipfile
+import io
+from requests.exceptions import ConnectionError as ReqConnectionError, HTTPError, InvalidSchema, InvalidURL, ReadTimeout
+from zeep.wsse.username import UsernameToken
+from zeep import Client, Settings
+from zeep.transports import Transport
+from lxml import etree
+
+
+_DATOS = {'ruc' : '20558326256','user' : 'RICARDO1', 'password' : 'Balti19','doc_type':'01'}
+
+def _unzip_edi_document(z_str):
+        buffer = io.BytesIO(z_str)
+        zipfile_obj = zipfile.ZipFile(buffer)
+        filenames = zipfile_obj.namelist()
+        xml_filenames = [x for x in filenames if x.endswith(('.xml', '.XML'))]
+        filename_to_decode = xml_filenames[0] if xml_filenames else filenames[0]
+        content = zipfile_obj.read(filename_to_decode)
+        buffer.close()
+        return content
+
+def decode_response(soap_response):
+    response_tree = etree.fromstring(soap_response)
+    if response_tree.find('.//{*}Fault') is not None:
+        return {}
+    if response_tree.find('.//{*}sendBillResponse') is not None:
+        cdr_b64 = response_tree.find('.//{*}applicationResponse').text
+        cdr = _unzip_edi_document(base64.b64decode(cdr_b64))
+        return {'cdr': cdr}
+    if response_tree.find('.//{*}getStatusResponse') is not None:
+        code = response_tree.find('.//{*}statusCode').text
+        if response_tree.find('.//{*}content') is not None:
+            cdr_b64 = response_tree.find('.//{*}content').text
+        else:
+            cdr = None
+        return {'code': code, 'cdr': cdr}
+    if response_tree.find('.//{*}sendSummaryResponse') is not None:
+        ticket = response_tree.find('.//{*}ticket').text
+        return {'number': ticket}
+    if response_tree.find('.//{*}getStatusCdrResponse') is not None:
+        code = response_tree.find('.//{*}statusCode').text
+        message = response_tree.find('.//{*}statusMessage').text
+        if response_tree.find('.//{*}content') is not None:
+            cdr_b64 = response_tree.find('.//{*}content').text
+            cdr = _unzip_edi_document(base64.b64decode(cdr_b64))
+            return {'code': code, 'message': message, 'cdr': cdr}
+        else:
+            return {}
+    return {}
+
+def get_sunat_credentials():
+        
+        res = {'fault_ns': 'soap-env'}
+        res.update({
+            'wsdl': 'https://e-factura.sunat.gob.pe/ol-it-wsconscpegem/billConsultService?wsdl',
+            # 'wsdl': _get_sunat_wsdl_test(),
+            'token': UsernameToken(_DATOS['ruc']+_DATOS['user'], _DATOS['password']),
+        })
+        return res
+
+def get_status_cdr_sunat_service(serie, number):
+    credentials = get_sunat_credentials()
+    transport = Transport(operation_timeout=15, timeout=15)
+    try:
+        settings = Settings(raw_response=True)
+        client = Client(
+            wsdl=credentials['wsdl'],
+            wsse=credentials['token'],
+            transport=transport,
+            settings=settings,
+            )
+        result = client.service.getStatusCdr(_DATOS['ruc'], _DATOS['doc_type'], serie, number)
+        result.raise_for_status()
+    except (ReqConnectionError, HTTPError, InvalidSchema, InvalidURL, ReadTimeout) as e:
+        print(e)
+        return False
+    soap_response = result.content
+    soap_response_decoded = decode_response(soap_response) if soap_response else {}
+    if soap_response_decoded.get('error'):
+        return {'error': soap_response_decoded['error'], 'blocking_level': 'error', 'code': soap_response_decoded.get('code')}
+
+    code = soap_response_decoded.get('code')
+    status = '%s|%s' % (code, soap_response_decoded.get('message'))
+    cdr = soap_response_decoded.get('cdr')
+    ## guardar cdr en archivo
+    if cdr:
+        ## nombre del archivo con ruc y numero de comprobante
+        path = os.path.dirname(os.path.abspath(__file__))
+        file_name = 'CDR-'+_DATOS['ruc'] + '-' + serie + '-' + number + '.xml'
+
+        with open(os.path.join(path,file_name), 'wb') as f:
+            f.write(cdr)
+    return {'cdr': cdr, 'status': status, 'code': code}
+
+if __name__ == "__main__":
+    # solicitar serie y numero de comprobante
+    serie = input("Ingrese serie : ")
+    serie = serie.strip()
+    lst_nums = input("Ingrese numeros separados por comas [,] : ")
+    lst_nums = lst_nums.split(',')
+    for num in lst_nums:
+        response = get_status_cdr_sunat_service(serie, num)
+        if response:
+            print(response['status'], response['code'])
+        print(response)
